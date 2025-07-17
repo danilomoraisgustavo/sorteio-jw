1f255aab2f03b19207e2310d9c89bb60cc07c801
PyDenTech <pydentech@gmail.com>
2025-07-17 18:21:11 -0300

    atualizacao de porta

--- server.js
+++ server.js
@@ -68,11 +68,11 @@ db.serialize(() => {
                 stmt.run(ch.name, ch.gender);
             }
             stmt.finalize(() => {
-                const PORT = process.env.PORT || 3000;
+                const PORT = process.env.PORT || 5000;
                 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
             });
         } else {
-            const PORT = process.env.PORT || 3000;
+            const PORT = process.env.PORT || 5000;
             app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
         }
     });
